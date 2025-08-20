1c74113a3099216807a2f3a5deb51b617ca7c685
Victor Fausto <94501287+JoaumVictor@users.noreply.github.com>
2025-08-19 21:41:41 -0300

    fix: user model to login

--- mobs-api/app/Models/User.php
+++ mobs-api/app/Models/User.php
@@ -7,7 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+// Importe a interface JWTSubject aqui
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+// Adicione "implements JWTSubject" aqui
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -43,5 +47,25 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
